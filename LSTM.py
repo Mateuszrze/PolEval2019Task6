34f18f31ccd6d827c63e984cce4ee573a919533e
Rzepa <mateuszrzepecki.99@gmail.com>
2023-06-20 17:00:44 +0200

    LSTM version ready to hyperparameter tuning (notebook has partial tuning results)

--- LSTM.py
+++ LSTM.py
@@ -9,7 +9,7 @@ import torch.optim as optim
 # first comment to the model: it learned that returning 0 is a good solution and kept doing that all the time. 
 # Thus, we altered the dataset to contain the same amount of results for 0, 1 and 2
 class LSTMModel(nn.Module):
-    def __init__(self, size = 256, embedding_dim = 100, num_layers = 2, dropout = 0.2, number_of_output_classes = 2, L = 30, prediction_method = 'sum'):
+    def __init__(self, size = 256, embedding_dim = 100, num_layers = 2, dropout = 0.2, number_of_output_classes = 2, L = 30, prediction_method = 'sum', learning_rate=0.0001):
         super(LSTMModel, self).__init__()
         self.lstm_size = size
         self.embedding_dim = embedding_dim
@@ -29,7 +29,10 @@ class LSTMModel(nn.Module):
            self.postprocess_predictions = self.compute_predictions3
         if prediction_method == 'max':
            self.postprocess_predictions = self.compute_predictions
-        #self.init_params()
+        self.epoch = 0
+        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=2, gamma=0.9)
+        self.criterion = nn.CrossEntropyLoss()
 
     def forward(self, x, prev_state):
         output, state = self.lstm(x, prev_state)
@@ -72,12 +75,9 @@ class LSTMModel(nn.Module):
         predictions, (_, _) = self(x, (state_h, state_c))
         return torch.argmax(self.postprocess_predictions(predictions), dim = 1)
     
-    def train(self, gen_batched, epochs, lr):
-        optimizer = optim.Adam(self.parameters(), lr=lr)
-        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.9)
-
-        criterion = nn.CrossEntropyLoss()
-    
+    def train_model(self, gen_batched, epochs):
+        self.train()
+        
         for epoch in range(epochs):
             text, tags = gen_batched()
             tot_loss = 0
@@ -85,18 +85,19 @@ class LSTMModel(nn.Module):
             for (input_batch, true_classes) in zip(text, tags):
                 state_h, state_c = self.init_state()
                 y_pred, (state_h, state_c) = self(input_batch, (state_h, state_c))
-                loss = criterion(self.postprocess_predictions(y_pred), true_classes)
+                loss = self.criterion(self.postprocess_predictions(y_pred), true_classes)
                 tot_loss += loss
                 losses.append(float(loss))
 
                 state_h = state_h.detach()
                 state_c = state_c.detach()            
 
-                optimizer.zero_grad()
+                self.optimizer.zero_grad()
                 loss.backward()
-                optimizer.step()
+                self.optimizer.step()
 
-            print(f'After epoch {epoch} tot_loss = {tot_loss}')
-            scheduler.step()
+            print(f'After epoch {self.epoch} tot_loss = {tot_loss}', end = '')
+            self.scheduler.step()
+            self.epoch += 1
                 
         
